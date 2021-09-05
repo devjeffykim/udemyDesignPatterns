58fbd85584e1a10dabf8172746278a303ccfdfcb
devjeff <devjeffkimj3@gmail.com>
2021-09-05 12:13:27 -0700

    Adding command and factory method examples

---
+++ Command/Example/FileInvoker.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+class FileInvoker {
+    public Command command;
+
+    public FileInvoker(Command c) {
+        command = c;
+    }
+
+    public void execute() {
+        command.execute();
+    }
+}
